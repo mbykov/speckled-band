95ee55e07e0ee4298fb5bb3a81de1e3e3c9a0e54
Michael Bykov <m.bykov@gmail.com>
2019-04-20 23:46:36 +0300

    v.3.1.1

--- src/index.js
+++ src/index.js
@@ -12,6 +12,10 @@ let d = debug
 // greek 0370-03FF 1F00–1FFF
 // diactitic 0300-036F
 // tibetan shad - ། U+0F0D : TIBETAN MARK SHAD {shey} // punctuation
+// right single quotation mark - ' - \u2019
+// left single quotation mark - ' - \u2018
+// general punctuations \u2000-\u206F
+
 // common punctuation in this version of sband
 let puncts = '([\u002E\u002C\u0021\u003A\u003B\u00B7\u0F0D])'
 
@@ -19,7 +23,7 @@ let puncts = '([\u002E\u002C\u0021\u003A\u003B\u00B7\u0F0D])'
 let coderanges = {
   'zho': '([\u4E00-\u9FFF]+)',
   'tib': '([\u0F00-\u0FFF]+)',
-  'grc': '([\u0370-\u03FF\u1F00-\u1FFF\u0300-\u036F\u1fbd\u0027]+)' // note: grc not includes space - \u0020, but includes apostrophe \u0027
+  'grc': '([\u0370-\u03FF\u1F00-\u1FFF\u0300-\u036F\u1fbd\u2019]+)' // note: grc not includes space - \u0020, and not includes apostrophe \u0027
 }
 
 let spaces = ['\u0020']
